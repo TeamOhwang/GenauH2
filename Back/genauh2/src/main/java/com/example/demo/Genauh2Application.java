48f67cdf5bfc2677648042e91d9bb00c6f980739
dkfmrhs170@gmail.com <137868291+naraeha@users.noreply.github.com>
2025-08-18 09:36:42 +0900

    프로젝트 생성

---
+++ Back/genauh2/src/main/java/com/example/demo/Genauh2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Genauh2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Genauh2Application.class, args);
+	}
+
+}
