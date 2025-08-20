998be56773323a6c952afbcd789791a29dab7960
dkfmrhs170@gmail.com <137868291+naraeha@users.noreply.github.com>
2025-08-20 11:31:28 +0900

    설비 등록, 조회 구현

--- Back/genauh2/src/main/java/com/project/controller/FacilitiesController.java
+++ Back/genauh2/src/main/java/com/project/controller/FacilitiesController.java
@@ -1,21 +1,43 @@
 package com.project.controller;
 
+import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.entity.Facility;
-
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import com.project.service.FacilityService;
 
 @RestController
 @RequestMapping("/plant")
 public class FacilitiesController {
-	@GetMapping("/insert")
-	public ResponseEntity<Map<String, Object>> facInsert(@RequestBody Facility fac){
-		return null;
-	}
+    
+    @Autowired
+    private FacilityService facilityService;
+    
+    @PostMapping("/insert")
+    public ResponseEntity<Map<String, Object>> facInsert(@RequestBody Facility facility) {
+        Facility savedFacility = facilityService.saveFacility(facility);
+        return ResponseEntity.ok(Map.of("success", true, "data", savedFacility));
+    }
+    
+    @GetMapping("/list")
+    public ResponseEntity<Map<String, Object>> facList(@RequestParam(required = false) Long orgId) {
+        List<Facility> facilities;
+        
+        if (orgId != null) {
+            facilities = facilityService.getFacilitiesByOrgId(orgId);
+        } else {
+            facilities = facilityService.getAllFacilities();
+        }
+        
+        return ResponseEntity.ok(Map.of("success", true, "data", facilities));
+    }
 }
--- Back/genauh2/src/main/java/com/project/repository/FacilityRepository.java
+++ Back/genauh2/src/main/java/com/project/repository/FacilityRepository.java
@@ -1,5 +1,7 @@
 package com.project.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import com.project.entity.Facility;
 
 @Repository
 public interface FacilityRepository extends JpaRepository<Facility, Long> {
-
+    
+    List<Facility> findByOrgId(Long orgId);
 }
