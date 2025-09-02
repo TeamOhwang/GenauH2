09de4278f8bba7213f4d6c2b160142899ca46377
dkfmrhs170@gmail.com <137868291+naraeha@users.noreply.github.com>
2025-09-02 11:34:27 +0900

    Update EmailService.java

--- Back/genauh2/src/main/java/com/project/service/EmailService.java
+++ Back/genauh2/src/main/java/com/project/service/EmailService.java
@@ -35,7 +35,7 @@ public class EmailService {
             helper.setSubject("[GENAUH2] 비밀번호 재설정 요청");
             
             // 프론트엔드 경로로 수정
-            String resetLink = "http://115.68.194.84:5174/changePassword?token=" + resetToken;
+            String resetLink = "http://localhost:5174/changePassword?token=" + resetToken;
             
             String htmlContent = createPasswordResetEmailTemplate(userName, resetLink, resetToken);
             helper.setText(htmlContent, true);
